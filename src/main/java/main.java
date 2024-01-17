d0e1e100790dbe20f01bb7d71b2106f4267b45ab
guillemate <156332187+guillemate@users.noreply.github.com>
2024-01-17 14:58:23 +0100

    ejercicio ejem git2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
 
-        System.out.print("introduce un numero: ");
+        System.out.print("introduce un numero:  ");
         Scanner lectura = new Scanner(System.in);
         int n = lectura.nextInt();
         String modaba = "y modaba";
